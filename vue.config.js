ba66f40ac07fa838b690ccff0275bc208f73659f
z825316969 <825316969@qq.com>
2019-07-03 18:20:12 +0800

    set rem

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const px2rem = require('postcss-px2rem')
 
 const postcss = px2rem({
-  remUnit: 32   //基准大小 baseSize，需要和rem.js中相同
+  remUnit: 16  //基准大小 baseSize，需要和rem.js中相同
 })
 
 module.exports = {
